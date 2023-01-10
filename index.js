79a1b2764548eda121064ad319b9ffe2621c1fbb
Benoit Potty <benoit.potty@liip.ch>
2023-01-10 11:22:50 +0100

    Improve StorageAdapter to serve files trough a source_file stack

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ class RokkaAdapter extends BaseAdapter {
     logging.debug(`Rokka configuration: ${JSON.stringify(outputOptions)}`);
     this.org = options.organization
     this.defaultStack = options.defaultStack || 'dynamic/o-af-1'
+    this.sourceFileStack = options.sourceFileStack || 'source_file'
+    this.rawFileExtensions = options.rawFileExtensions?.split(',') || ['mp3']
     this.rokka = rokka({apiKey: config.key || ''})
     this.addFaceDetection = options.addFaceDetection || false
     logging.info('Rokka Storage Adapter loaded');
@@ -27,8 +29,17 @@ class RokkaAdapter extends BaseAdapter {
      // TBD: Not sure it's actually needed.
   }
 
-  async save(image, noFace) {
-    const stream = fs.createReadStream(image.path)
+  _defineStackToUse(fileName) {
+    if (!fileName) {
+        return this.defaultStack;
+    }
+
+    const extension = fileName.split('.').pop();
+    return this.rawFileExtensions.includes(extension) ? this.sourceFileStack : this.defaultStack;
+  } 
+
+  async save(file, noFace) {
+    const stream = fs.createReadStream(file.path)
     return new Promise((resolve, reject) => {
       const meta = {
         meta_user: {'tool': 'ghost'}
@@ -36,9 +47,13 @@ class RokkaAdapter extends BaseAdapter {
       if (this.addFaceDetection && !noFace) {
         meta["meta_dynamic"] = {'detection_face': {}}
       }
-      this.rokka.sourceimages.create(this.org, image.originalname, stream, meta).then(res => {
+      
+      const fileName = file.originalname ?? file.name
+
+      this.rokka.sourceimages.create(this.org, fileName, stream, meta).then(res => {
         const rokkaImage = res.body.items[0]
-        const link = 'https://' + this.org + '.rokka.io/' + this.defaultStack + '/' + rokkaImage.short_hash + '/' + encodeURIComponent(
+        const stackToUse = this.defineStackToUse(fileName);
+        const link = 'https://' + this.org + '.rokka.io/' + stackToUse + '/' + rokkaImage.short_hash + '/' + encodeURIComponent(
           rokkaImage.name.replace(/\.[a-zA-Z]{3,4}$/,"").
           replace(/[.\-]/g,"_")
         ) + '.' + rokkaImage.format
@@ -49,7 +64,7 @@ class RokkaAdapter extends BaseAdapter {
         debug("error", err)
         // try without face, maybe there's an error there
         if (this.addFaceDetection && !noFace) {
-          this.save(image, true)
+          this.save(file, true)
         } else {
           reject(err)
         }
