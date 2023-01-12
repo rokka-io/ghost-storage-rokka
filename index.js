622f1df870b24c8ef343a47bc901391d0b0e7cd5
Benoit Potty <benoit.potty@liip.ch>
2023-01-12 10:24:42 +0100

    Do not request FaceDetection for raw files

--- index.js
+++ index.js
@@ -1,107 +1,102 @@
 /* eslint-disable no-promise-executor-return */
 /* eslint-disable max-lines */
-'use strict'
+'use strict';
 
-const BaseAdapter = require('ghost-storage-base')
+const BaseAdapter = require('ghost-storage-base');
 const logging = require('@tryghost/logging');
-const rokka = require('rokka')
-const fs = require("fs")
-const request = require('request').defaults({encoding: null})
-
+const rokka = require('rokka');
+const fs = require('fs');
+const request = require('request').defaults({encoding: null});
 
 class RokkaAdapter extends BaseAdapter {
-  constructor(options) {
-    super(options)
-    const config = options || {}
+    constructor(options) {
+        super(options);
+        const config = options || {};
 
-    // clone options and overwrite key for not leaking that to logs
-    const outputOptions = Object.assign({}, options)
-    outputOptions.key = "******"
-    logging.debug(`Rokka configuration: ${JSON.stringify(outputOptions)}`);
-    this.org = options.organization
-    this.defaultStack = options.defaultStack || 'dynamic/o-af-1'
-    this.sourceFileStack = options.sourceFileStack || 'source_file'
-    this.rawFileExtensions = options.rawFileExtensions?.split(',') || ['mp3', 'wav', 'ogg', 'm4a', 'mp4', 'webm', 'ogv']
-    this.rokka = rokka({apiKey: config.key || ''})
-    this.addFaceDetection = options.addFaceDetection || false
-    logging.info('Rokka Storage Adapter loaded');
-  }
-
-  exists() {
-    //Rokka handles this already.
-    return false;
-  }
-
-  urlToPath() {
-    //Rokka stores element flat.
-    return '/';
-  }
-
-  _defineStackToUse(fileName) {
-    if (!fileName) {
-        return this.defaultStack;
+        // clone options and overwrite key for not leaking that to logs
+        const outputOptions = Object.assign({}, options);
+        outputOptions.key = '******';
+        logging.debug(`Rokka configuration: ${JSON.stringify(outputOptions)}`);
+        this.org = options.organization;
+        this.defaultStack = options.defaultStack || 'dynamic/o-af-1';
+        this.sourceFileStack = options.sourceFileStack || 'source_file';
+        this.rawFileExtensions = options.rawFileExtensions?.split(',') || ['mp3', 'wav', 'ogg', 'm4a', 'mp4', 'webm', 'ogv'];
+        this.rokka = rokka({apiKey: config.key || ''});
+        this.addFaceDetection = options.addFaceDetection || false;
+        logging.info('Rokka Storage Adapter loaded');
     }
 
-    const extension = fileName.split('.').pop();
-    return this.rawFileExtensions.includes(extension) ? this.sourceFileStack : this.defaultStack;
-  } 
+    exists() {
+        //Rokka handles this already.
+        return Promise.resolve(false);
+    }
 
-  async save(file, noFace) {
-    const stream = fs.createReadStream(file.path)
-    return new Promise((resolve, reject) => {
-      const meta = {
-        meta_user: {'tool': 'ghost'}
-      }
-      if (this.addFaceDetection && !noFace) {
-        meta["meta_dynamic"] = {'detection_face': {}}
-      }
-      
-      const fileName = file.originalname ?? file.name
+    urlToPath() {
+        //Rokka stores element flat.
+        return '/';
+    }
 
-      this.rokka.sourceimages.create(this.org, fileName, stream, meta).then(res => {
-        const rokkaImage = res.body.items[0]
-        const stackToUse = this._defineStackToUse(fileName);
-        const link = 'https://' + this.org + '.rokka.io/' + stackToUse + '/' + rokkaImage.short_hash + '/' + encodeURIComponent(
-          rokkaImage.name.replace(/\.[a-zA-Z]{3,4}$/,"").
-          replace(/[.\-]/g,"_")
-        ) + '.' + rokkaImage.format
-        logging.info(`File Uploaded and accessible at: ${link}`);
-        resolve(link)
-
-      }).catch(err => {
-        logging.debug(`Error: ${JSON.stringify(err)}`)
-        // try without face, maybe there's an error there
-        if (this.addFaceDetection && !noFace) {
-          this.save(file, true)
-        } else {
-          reject(err)
+    _isRawFile(fileName) {
+        if (!fileName) {
+            return false;
         }
-      })
-    })
-  }
 
-  serve() {
-    return (req, res, next) => {
-      next()
+        const extension = fileName.split('.').pop();
+        return this.rawFileExtensions.includes(extension);
     }
-  }
 
-  delete() {
-    // Let Ghost believe that the file has been delete. Rokka manages it
-  }
+    async save(file) {
+        const stream = fs.createReadStream(file.path);
+        return new Promise((resolve, reject) => {
+            const fileName = file.originalname ?? file.name;
+            const isRawFile = this._isRawFile(fileName);
+            const meta = {
+                meta_user: {tool: 'ghost'}
+            };
+            if (!isRawFile && this.addFaceDetection) {
+                logging.debug(`Face detection requested for file ${fileName}. It is not a raw file, and 'addFaceDetection' setting is active`);
+                meta.meta_dynamic = {detection_face: {}};
+            }
 
-  read(options) {
-    const opts = options || {}
-    return new Promise((resolve, reject) => request.get(opts.path, (err, res) => {
-      if (err) {
-        return reject({
-          err: err,
-          message: `Could not read image ${opts.path}`
-        })
-      }
-      return resolve(res.body)
-    }))
-  }
+            this.rokka.sourceimages.create(this.org, fileName, stream, meta).then((res) => {
+                const rokkaImage = res.body.items[0];
+                const stackToUse = isRawFile ? this.sourceFileStack : this.defaultStack;
+                const link = 'https://' + this.org + '.rokka.io/' + stackToUse + '/' + rokkaImage.short_hash + '/' + encodeURIComponent(
+                    rokkaImage.name.replace(/\.[a-zA-Z]{3,4}$/,'')
+                        .replace(/[.-]/g,'_')
+                ) + '.' + rokkaImage.format;
+                logging.info(`File uploaded and accessible at: ${link}`);
+                resolve(link);
+            }).catch((err) => {
+                logging.error(`Error while uploading. Reason: ${JSON.stringify(err)}`);
+                reject(err);
+            });
+        });
+    }
+
+    serve() {
+        return (_req, _res, next) => {
+            next();
+        };
+    }
+
+    delete() {
+        // Let Ghost believe that the file has been delete. Rokka manages it
+        return Promise.resolve(true);
+    }
+
+    read(options) {
+        const opts = options || {};
+        return new Promise((resolve, reject) => request.get(opts.path, (err, res) => {
+            if (err) {
+                return reject({
+                    err: err,
+                    message: `Could not read image ${opts.path}`
+                });
+            }
+            return resolve(res.body);
+        }));
+    }
 }
 
-module.exports = RokkaAdapter
+module.exports = RokkaAdapter;
